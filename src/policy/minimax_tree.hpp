484063ea07e9965566f8b11f0d0058e0cb572746
SwankyVito <136663005+SwankyVito@users.noreply.github.com>
2023-06-17 22:00:58 +0800

    Merge branch 'main' into class-Tree-aproach

---
+++ src/policy/minimax_tree.hpp
@@ -0,0 +1,19 @@
+#ifndef __TREE_H__
+#define __TREE_H__
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <utility>
+#include <map>
+#include "../config.hpp"
+#include "../state/state.hpp"
+
+class Tree{
+    public:
+        Tree(){}
+        int minimax();
+        std::map<std::pair<int,State>,std::vector<State>> tree;
+
+};
+
+#endif
