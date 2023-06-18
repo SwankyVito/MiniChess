9d772a34315fd3e062abc7030279664bf42cbd74
SwankyVito <136663005+SwankyVito@users.noreply.github.com>
2023-06-18 22:30:54 +0800

    update: minimax function

--- src/policy/random2.cpp
+++ src/policy/random2.cpp
@@ -2,6 +2,7 @@
 
 #include "../state/state.hpp"
 #include "./random2.hpp"
+#include "../minimax/minimax.hpp"
 
 /**
  * @brief Randomly get a legal action
@@ -16,6 +17,17 @@ Move Random2::get_move(State *state, int depth){
     state->get_legal_State();
   }
   auto actions = state->legal_actions;
+  Move select;
+  int point = MIN;
+  for(auto move : state->legal_actions){
+    State *next = state->next_state(move);
+    minimax cal;
+    int tmp = cal.do_minimax(next,1,true,MIN,MAX);
+    if( tmp >= point){
+      select = move;
+      point = tmp;
+    }
+  }
   //temporary selections
-  return actions[(rand()+depth)%actions.size()];
+  return select;
 }
--- src/policy/random2.hpp
+++ src/policy/random2.hpp
@@ -1,9 +1,8 @@
 #pragma once
 #include "../state/state.hpp"
+#include "../minimax/minimax.hpp"
 
-const int MAX = 1028;
-const int MIN = -1028;
-/**
+/*
  * @brief Policy class for random policy, 
  * your policy class should have get_move method
  */
@@ -11,34 +10,5 @@ class Random2{
 public:
   static Move get_move(State *state, int depth);
   int max_depth;
-  int minimax(State state, int depth, bool maxplayer, int alpha, int beta){
-    if(depth == max_depth || state.game_state == WIN){
-      return state.evaluate();
-    }
-    if(maxplayer){
-      int best = MIN;
-      for(auto move: state.legal_actions){
-        State next = *(state.next_state(move));
-        int val = minimax(next, depth+1, false, alpha, beta);
-        best = std::max(best ,val);
-        alpha = std::max(alpha ,best);
-        if(beta<=alpha)
-          break;
-      }
-
-      return best;
-    }
-    else{
-      int best = MAX;
-      for(auto move: state.legal_actions){
-        State next = *(state.next_state(move));
-        int val = minimax(next, depth+1, true, alpha, beta);
-        best = std::min(best ,val);
-        beta = std::min(beta ,best);
-        if(beta<=alpha)
-          break;
-      }
-      return best;
-    }
-  }
+  
 };
